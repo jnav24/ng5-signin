1c3727d140e0cb0e381b6511d23e6068f0274f65
Justin Navarro <justinn@liveanswer.com>
2018-04-07 17:47:11 -0400

    sass is working and converted login form material design markup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {RouterModule} from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+    MatButtonModule, MatCardModule, MatCheckboxModule, MatIconModule, MatInputModule, MatMenuModule, MatRippleModule,
+    MatToolbarModule
+} from '@angular/material';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 // Config
 import { RouterConfig } from '@app/config/router.config';
@@ -12,15 +18,26 @@ import { SignInComponent } from './sign-in/sign-in.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SignInComponent,
-  ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(RouterConfig),
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        SignInComponent,
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        MatInputModule,
+        MatRippleModule,
+        MatButtonModule,
+        MatCardModule,
+        MatCheckboxModule,
+        MatIconModule,
+        MatMenuModule,
+        MatToolbarModule,
+        ReactiveFormsModule,
+        RouterModule.forRoot(RouterConfig),
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-sign-in',
-  templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
+    selector: 'app-sign-in',
+    templateUrl: './sign-in.component.html',
+    styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent implements OnInit {
+    error: String = '';
+    log_in: FormGroup;
+    sign_up: FormGroup;
 
-  constructor() { }
+    constructor(private fb: FormBuilder) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.log_in = this.fb.group({
+            email: ['', [Validators.required, Validators.pattern(/\S+@\S+\.\S+/)]],
+            password: ['', [Validators.required]],
+        });
 
+        this.sign_up = this.fb.group({
+            first_name: ['', [Validators.required, Validators.minLength(3)]],
+            last_name: ['', [Validators.required, Validators.minLength(3)]],
+            email: ['', [Validators.required, Validators.pattern(/\S+@\S+\.\S+/)]],
+            password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(24)]],
+            confirm_password: ['', [Validators.required]],
+        });
+    }
 }
