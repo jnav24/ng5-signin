a7f40628f3ee36ce94b91fbe1a6c7203587cc1ff
Justin Navarro <justinn@liveanswer.com>
2018-05-16 23:13:40 -0400

    add another to unit test for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,7 +2,7 @@ import {TestBed, async, ComponentFixture, fakeAsync} from '@angular/core/testing
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { RouterConfig } from '@app/config/router.config';
-import {Router} from '@angular/router';
+import {Router, RouterOutlet} from '@angular/router';
 import {Location} from '@angular/common';
 import {SignInComponent} from '@app/sign-in/sign-in.component';
 import {DashboardComponent} from '@app/dashboard/dashboard.component';
@@ -11,6 +11,7 @@ import {
     MatButtonModule, MatCardModule, MatCheckboxModule, MatIconModule, MatInputModule, MatMenuModule, MatRippleModule,
     MatToolbarModule
 } from '@angular/material';
+import {By} from '@angular/platform-browser';
 
 describe('AppComponent', () => {
     let component: AppComponent;
@@ -52,6 +53,11 @@ describe('AppComponent', () => {
 
     it('should create an app', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should have a router outlet', () => {
+        const outlet = fixture.debugElement.query(By.directive(RouterOutlet));
+        expect(outlet).not.toBeNull();
     });
 
     it('navigate to "" redirects you to login page', <any>fakeAsync(() => {
