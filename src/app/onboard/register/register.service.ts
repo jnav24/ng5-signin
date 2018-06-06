9bb2e5b869697f7f97a36ff23c3cbf5460b6043f
Justin Navarro <justinn@liveanswer.com>
2018-06-06 08:06:39 -0400

    removed unused code

--- src/app/onboard/register/register.service.ts
+++ src/app/onboard/register/register.service.ts
@@ -1,18 +1,9 @@
 import { Injectable } from '@angular/core';
 import {AngularFireAuth} from 'angularfire2/auth';
-import {Observable} from 'rxjs/Observable';
-import {AngularFireDatabase} from 'angularfire2/database';
-import * as firebase from 'firebase/app';
 
 @Injectable()
 export class RegisterService {
-    user: Observable<any>;
-
-    constructor(
-        private af: AngularFireDatabase,
-        private auth: AngularFireAuth) {
-        this.user = auth.authState;
-    }
+    constructor(private auth: AngularFireAuth) {}
 
     createNewUser(email: string, pass: string): Promise<any> {
         return this.auth.auth.createUserWithEmailAndPassword(email, pass);
