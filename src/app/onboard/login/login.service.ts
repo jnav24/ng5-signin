cdf54c2025202505c5353d43aeee09fdeb6440ab
Justin Navarro <info@justinnavarro.net>
2018-06-08 21:55:36 -0400

    update save and remember token functionality

--- src/app/onboard/login/login.service.ts
+++ src/app/onboard/login/login.service.ts
@@ -5,6 +5,7 @@ import {AngularFireDatabase} from 'angularfire2/database';
 import {AngularFireAuth} from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
 import {FirebaseDbService} from '@app/common/services/firebase-db.service';
+import {AngularFirestore} from 'angularfire2/firestore';
 
 @Injectable()
 export class LoginService {
@@ -14,6 +15,7 @@ export class LoginService {
 
     constructor(
         private firebaseDb: FirebaseDbService,
+        private afs: AngularFirestore,
         private af: AngularFireDatabase,
         private auth: AngularFireAuth) {
         this.user = auth.authState;
@@ -36,6 +38,31 @@ export class LoginService {
         return this.auth.auth.signOut();
     }
 
+    removeToken(uid) {
+        let user;
+
+        if (this.firebaseDb.isFirebase()) {
+            user = this.af.object(`users/${uid}`);
+            return user.update({ token: '' });
+        }
+
+        user = this.afs.collection(`users`).doc(uid);
+        return user.update({ token: '' });
+    }
+
+    setRememberMe(uid) {
+        let user;
+        const data = { remember_me: true };
+
+        if (this.firebaseDb.isFirebase()) {
+            user = this.af.object(`users/${uid}`);
+            return user.update(data);
+        }
+
+        user = this.afs.collection('users').doc(uid);
+        return user.update(data);
+    }
+
     saveToken(token, uid) {
         let user;
 
@@ -44,22 +71,8 @@ export class LoginService {
             return user.update({ token: token});
         }
 
-        user = this.af.object(`users/${uid}`);
+        user = this.afs.collection(`users/`).doc(uid);
         return user.update({ token: token});
-    }
-
-    removeToken(uid) {
-        const user = this.af.object(`users/${uid}`);
-        return user.update({ token: '' });
-    }
-
-    getUser(uid) {
-        return this.af.object(`/users/${uid}`);
-    }
-
-    setRememberMe(uid) {
-        const user = this.af.object(`users/${uid}`);
-        return user.update({ remember_me: true });
     }
 
     redirectUser(user) {
