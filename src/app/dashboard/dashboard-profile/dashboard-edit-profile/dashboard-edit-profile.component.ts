9393c0d078d5e51297278e9f37b2446c53061a6a
Justin Navarro <info@justinnavarro.net>
2018-07-21 14:08:59 -0400

    set validation on edit profile page

--- src/app/dashboard/dashboard-profile/dashboard-edit-profile/dashboard-edit-profile.component.ts
+++ src/app/dashboard/dashboard-profile/dashboard-edit-profile/dashboard-edit-profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {UserInterface} from '@app/common/interfaces/user.interface';
+import {UsersService} from '@app/common/services/users.service';
 
 @Component({
     selector: 'app-edit-profile',
@@ -8,14 +10,32 @@ import {FormBuilder, FormGroup} from '@angular/forms';
 })
 export class DashboardEditProfileComponent implements OnInit {
     profile: FormGroup;
+    user: UserInterface;
 
-    constructor(private fb: FormBuilder) { }
+    constructor(private usersService: UsersService,
+                private fb: FormBuilder) { }
 
     ngOnInit() {
+        this.user = this.usersService.getUser();
+
         this.profile = this.fb.group({
-            fname: [],
-            lname: [],
-            email: []
+            first_name: ['', [Validators.required, Validators.minLength(3)]],
+            last_name: ['', [Validators.required, Validators.minLength(3)]],
+            email: ['', [Validators.required, Validators.pattern(/\S+@\S+\.\S+/)]]
         });
+
+        this.profile.setValue({
+            first_name: this.user.first_name,
+            last_name: this.user.last_name,
+            email: this.user.email
+        });
+    }
+
+    disallowSave() {
+        const matches = Object.keys(this.profile.value).filter(key => {
+            return this.user[key] === this.profile.value[key];
+        });
+
+        return matches.length === Object.keys(this.profile.value).length || !this.profile.valid;
     }
 }
